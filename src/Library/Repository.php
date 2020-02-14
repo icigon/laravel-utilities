90df887194d85cff08f06cf16e7dbf8aa9880f4f
icigon <singkek@icigon.com>
2020-02-14 14:32:53 +0800

    init

---
+++ src/Library/Repository.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package     IdnPlay\Utils\Library\Repository
+ * @author      singkek
+ * @copyright   Copyright(c) 2019
+ * @version     1
+ * @created     2020-02-07
+ * @updated     2020-02-07
+ **/
+namespace Icigon\Laravel\Utils\Library;
+
+class Repository
+{
+    use \Icigon\Laravel\Utils\Bus\Repository;
+}
+
